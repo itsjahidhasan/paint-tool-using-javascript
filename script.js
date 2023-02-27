db773d8e1384b8e4fa75b0148dbf2c7caeaa0064
itsjahidhasan <mdjahidhasanf92@gmail.com>
2023-02-28 00:22:00 +0600

    Canvas is added

---
+++ script.js
@@ -0,0 +1,97 @@
+const canvas = document.querySelector('canvas'),
+  toolBtns = document.querySelectorAll('.tool'),
+  fillColor = document.querySelector('#fill-color'),
+  sizeSlider = document.querySelector('#size-slider'),
+  ctx = canvas.getContext('2d');
+
+let prevMouseX,
+  prevMouseY,
+  snapshot,
+  isDrawing = false,
+  selectedTool = 'brush',
+  brushWidth = 5;
+
+window.addEventListener('load', () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+const drawRect = (e) => {
+  if (!fillColor.checked) {
+    return ctx.strokeRect(
+      e.offsetX,
+      e.offsetY,
+      prevMouseX - e.offsetX,
+      prevMouseY - e.offsetY
+    );
+  }
+  ctx.fillRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
+
+const drawCircle = (e) => {
+  ctx.beginPath();
+  let radius = Math.sqrt(
+    Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2)
+  );
+  ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  ctx.stroke();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const startDraw = (e) => {
+  isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+const drawing = (e) => {
+  if (!isDrawing) return;
+  ctx.putImageData(snapshot, 0, 0);
+
+  if (selectedTool === 'brush') {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  } else if (selectedTool === 'rectangle') {
+    drawRect(e);
+  } else if (selectedTool === 'circle') {
+    drawCircle(e);
+  } else {
+    drawTriangle(e);
+  }
+};
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.options .active').classList.remove('active');
+    btn.classList.add('active');
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
+sizeSlider.addEventListener('change', () => (brushWidth = sizeSlider.value));
+
+canvas.addEventListener('mousedown', startDraw);
+
+canvas.addEventListener('mousemove', drawing);
+
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+
+// 39.30
